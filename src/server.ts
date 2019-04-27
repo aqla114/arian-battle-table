1412700630690e028c494dab386881a0bb47ffde
h.miyazaki <aqla114@yahoo.co.jp>
2019-04-27 20:50:07 +0900

    KoaとPugとStylusと他諸々の連携ができた

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,24 @@
-import * as fs from 'fs';
 import * as path from 'path';
 import * as Koa from 'koa';
+import * as Pug from 'koa-pug';
 import * as serve from 'koa-static';
 
 const WORKDIR: string = '/workdir/dst';
 
 const app = new Koa();
 
-app.use(async (ctx, next) => {
-    const url = ctx.url || '';
-    const tmp = url.split('.');
-    const extention = tmp[tmp.length - 1];
+app.use(serve(WORKDIR));
 
-    switch (extention) {
-        case 'js':
-            ctx.type = 'text/javascript';
-            ctx.status = 200;
-            ctx.body = fs.readFileSync(path.join(WORKDIR, url));
-            break;
-        case 'css':
-            ctx.type = 'text/css';
-            ctx.status = 200;
-            ctx.body = fs.readFileSync(path.join(WORKDIR, url));
-            break;
-        case '/':
-            ctx.type = 'text/html';
-            ctx.status = 200;
-            ctx.body = fs.readFileSync(path.join(WORKDIR, 'index.html'));
+const pug = new Pug({
+    app: app,
+    viewPath: path.join(WORKDIR, 'template'),
+});
 
-            break;
-    }
+pug.use(app);
+
+app.use((ctx, _) => {
+    ctx.status = 200;
+    ctx.render('index');
 });
 
 app.listen(8000);
