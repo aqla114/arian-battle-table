c8a00d53224aa0673578e0dc09788127780b2e45
h.miyazaki <aqla114@yahoo.co.jp>
2020-09-03 21:46:46 +0900

    Add Component ButtonDropdown

---
+++ src/frontend/components/atoms/button-dropdown.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+
+export type ButtonDropdownValue = string | number;
+type Options = Array<ButtonDropdownValue>;
+
+type Props = {
+    name: string;
+    label: string;
+    value: ButtonDropdownValue;
+    options: Options;
+    onClick: OnClickDropdownListItem;
+};
+
+export const ButtonDropdown: React.SFC<Props> = (props: Props) => {
+    const [open, toggleOpen] = React.useState(false);
+
+    React.useEffect(() => {
+        const handler = (e: MouseEvent) => {
+            open && toggleOpen(false);
+        };
+
+        document.addEventListener('click', handler);
+
+        return () => document.removeEventListener('click', handler);
+    });
+
+    console.log(props.value);
+
+    return (
+        <div className="button-dropdown">
+            <button
+                className={`button-dropdown__button ${Boolean(props.value) ? '--active' : ''}`}
+                onClick={() => toggleOpen(!open)}
+            >{`${props.label}`}</button>
+            {open && (
+                <DropdownOptions
+                    options={props.options}
+                    dropdownKey={props.name}
+                    onClickListItem={(key, value) => props.onClick(key, value)}
+                />
+            )}
+        </div>
+    );
+};
+
+export type OnClickDropdownListItem = (key: string, value: ButtonDropdownValue) => void;
+
+const DropdownOptions: React.SFC<{
+    options: Options;
+    dropdownKey: string;
+    onClickListItem: OnClickDropdownListItem;
+}> = ({ options, dropdownKey, onClickListItem }) => {
+    return (
+        <ul className="dropdown-list">
+            {options.map(option => (
+                <li
+                    className="dropdown-list__item"
+                    key={option.toString()}
+                    onClick={_ => onClickListItem(dropdownKey, option)}
+                >
+                    {option}
+                </li>
+            ))}
+        </ul>
+    );
+};
