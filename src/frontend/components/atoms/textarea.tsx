96b1329a86c72d22f0f523a6e730c2ec4507128f
h.miyazaki <aqla114@yahoo.co.jp>
2020-09-05 21:08:05 +0900

    Add <Textarea />

---
+++ src/frontend/components/atoms/textarea.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+type Props = {
+    name: string;
+    value: string | number | string[];
+    className?: string;
+    showBorder?: boolean;
+    placeholder?: string;
+    onChange: (e: React.ChangeEvent<HTMLTextAreaElement>) => void;
+};
+
+export const Textarea: React.SFC<Props> = ({ showBorder = true, ...props }: Props) => {
+    return <textarea {...props} className={`textarea ${showBorder ? '--show-border' : ''} ${props.className}`} />;
+};
