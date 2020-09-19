494f206642a3cfe21cebfe8aeb2713b38e7cbacf
h.miyazaki <aqla114@yahoo.co.jp>
2020-09-19 21:49:05 +0900

    Add type skill

---
+++ src/frontend/types/skill.ts
@@ -0,0 +1,10 @@
+export type Skill = {
+    id?: number;
+    name: string;
+    timing: string;
+    detemination_way: string;
+    target: string;
+    range: string;
+    restriction: string;
+    detail: string;
+};
