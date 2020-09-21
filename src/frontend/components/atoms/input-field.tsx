c7fedf6031183edd6048eebe8540cac87d82a045
h.miyazaki <aqla114@yahoo.co.jp>
2020-09-21 11:39:43 +0900

    InputField に size を設定できるようにした

--- src/frontend/components/atoms/input-field.tsx
+++ src/frontend/components/atoms/input-field.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 type Kind = 'number' | 'text';
 type TextAlign = 'left' | 'right';
+type Size = 'small' | 'midddle' | 'large';
 
 type Props = {
     name: string;
@@ -11,10 +12,16 @@ type Props = {
     showBorder?: boolean;
     textAlign?: TextAlign;
     placeholder?: string;
+    size?: Size;
     onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-export const InputField: React.SFC<Props> = ({ textAlign = 'left', showBorder = true, ...props }: Props) => {
+export const InputField: React.SFC<Props> = ({
+    textAlign = 'left',
+    showBorder = true,
+    size = 'midddle',
+    ...props
+}: Props) => {
     const [curretntValue, setValue] = React.useState(props.value);
     React.useEffect(() => {
         setValue(props.value);
@@ -27,9 +34,9 @@ export const InputField: React.SFC<Props> = ({ textAlign = 'left', showBorder = 
             onChange={e => setValue(e.target.value)}
             onBlur={e => props.onChange(e)}
             type="text"
-            className={`input-field__${props.kind} ${`--${textAlign}`} ${showBorder ? '--show-border' : ''} ${
-                props.className
-            }`}
+            className={`input-field__${props.kind} ${`--${textAlign}`} ${`--${size}`} ${
+                showBorder ? '--show-border' : ''
+            } ${props.className}`}
         />
     );
 };
