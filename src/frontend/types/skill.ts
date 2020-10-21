e0751a01b1daa35b09e3c0cca08e175e23aeb3d6
h.miyazaki <aqla114@yahoo.co.jp>
2020-10-21 15:38:58 +0900

    Add Default Skill function

--- src/frontend/types/skill.ts
+++ src/frontend/types/skill.ts
@@ -8,3 +8,23 @@ export type Skill = {
     restriction: string;
     detail: string;
 };
+
+export function Skill(
+    name: string = '',
+    timing: string = '',
+    detemination_way: string = '',
+    target: string = '',
+    range: string = '',
+    restriction: string = '',
+    detail: string = '',
+): Skill {
+    return {
+        name,
+        timing,
+        detemination_way,
+        target,
+        range,
+        restriction,
+        detail,
+    };
+}
