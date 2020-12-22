7b8ac096d894a201be0db889bc0d84be146e4ae6
h.miyazaki <aqla114@yahoo.co.jp>
2020-12-22 20:01:06 +0900

    Add API : loadCharactersFromSheet

---
+++ src/server/api/load-characters-from-sheet.ts
@@ -0,0 +1,56 @@
+import * as Request from 'superagent';
+import { Repository } from 'typeorm';
+import { Character } from '../../types';
+import { Character as CharacterModel } from '../models/character';
+import { BattleSession } from '../models/battle-session';
+import { Context } from '../types';
+
+export async function loadCharactersFromSheet(ctx: Context) {
+    const battleSessionRepo: Repository<BattleSession> = ctx.ports.battleSession;
+
+    const req = ctx.request.body;
+    const { guildId, characters: currentCharacters }: { guildId: string; characters: CharacterModel[] } = req;
+
+    if (!guildId) {
+        console.error('Bad request.');
+        return;
+    }
+
+    const sessionId: string = (ctx as any).params['id'];
+
+    const battleSession = await battleSessionRepo.findOne({
+        where: { id: sessionId },
+        relations: ['characters', 'characters.badStatus', 'characters.skills'],
+    });
+
+    if (!battleSession) {
+        console.error('The battle session is not found.');
+        return;
+    }
+
+    const response = await Request.get(`load-characters-server:8001/guild/${guildId}`).catch(err => err);
+
+    if (!response || response.status !== 200) {
+        console.error(`Unable to load characters from sheets. SheetID is ${guildId}`);
+        return;
+    }
+
+    const characters = parseCharactersFromJson(response.body);
+
+    battleSession.characters = [...currentCharacters, ...characters];
+
+    const res = await battleSessionRepo.save(battleSession);
+
+    return res;
+}
+
+// load-character-server から飛んできたリクエストを Character の配列に変換する。
+function parseCharactersFromJson(jsonBody: any): CharacterModel[] {
+    if (!Array.isArray(jsonBody)) {
+        return [];
+    }
+
+    const characters = jsonBody.map((x: Partial<Character>) => CharacterModel.mk(Character(x)));
+
+    return characters;
+}
