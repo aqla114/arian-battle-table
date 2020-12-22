c87f28aa7680f8283153d561e728566d36c38e2f
h.miyazaki <aqla114@yahoo.co.jp>
2020-12-22 19:59:26 +0900

    Fix type : Character

--- src/types/character.ts
+++ src/types/character.ts
@@ -3,7 +3,7 @@ import { BadStatus, defaultBadStatus } from './bad-status';
 import { Skill } from './skill';
 
 export type Character = {
-    id: string;
+    id?: string;
     name: string;
     attribute: Attribute;
     defaultActionPriority: number;
@@ -34,36 +34,7 @@ export type Character = {
     skills: Skill[];
 };
 
-export function Character(
-    character: Partial<Character> = {
-        name: '',
-        attribute: 'None',
-        actionPriority: 0,
-        defaultActionPriority: 0,
-        hp: 0,
-        maxHp: 0,
-        physicalDefence: 0,
-        defaultPhysicalDefence: 0,
-        magicalDefence: 0,
-        defaultMagicalDefence: 0,
-        strength: 0,
-        strength_base: 0,
-        dexterity: 0,
-        dexterity_base: 0,
-        agility: 0,
-        agility_base: 0,
-        wisdom: 0,
-        wisdom_base: 0,
-        sensitivity: 0,
-        sensitivity_base: 0,
-        power: 0,
-        power_base: 0,
-        luck: 0,
-        luck_base: 0,
-        memo: '',
-        skills: [],
-    },
-): Omit<Character, 'id'> {
+export function Character(character: Partial<Character>): Omit<Character, 'id'> {
     return {
         name: '',
         attribute: 'None',
@@ -93,5 +64,6 @@ export function Character(
         skills: [],
         badStatus: defaultBadStatus,
         isActed: false,
+        ...character,
     };
 }
