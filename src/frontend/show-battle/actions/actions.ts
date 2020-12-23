c7ffc4fd62702a69aa2ea0695b60cb9fdbbbc99b
h.miyazaki <aqla114@yahoo.co.jp>
2020-12-23 21:00:59 +0900

    Add actions for importCharactersByGuildId

--- src/frontend/show-battle/actions/actions.ts
+++ src/frontend/show-battle/actions/actions.ts
@@ -5,16 +5,29 @@ import { saveCharactersNewlyActions } from './save-characters-newly';
 import { ButtonDropdownValue } from '../../components/atoms/button-dropdown';
 import { Character } from '../../types/character';
 import { loadSkillsCsvActions } from './load-skills-csv';
+import { importCharactersByGuildIdActions } from './import-characters-by-guild-id';
 
 export type CharacterID = string;
+export type GuildId = string;
 export type CharacterName = string;
 export type SkillName = string;
 
+// TODO 全部これ使う。
+export type ActionProps<S, T = undefined> = T extends undefined
+    ? {
+          e: S;
+      }
+    : {
+          e: S;
+          payload: T;
+      };
+
 export type ChangeSessionNameProps = { e: React.ChangeEvent<HTMLInputElement> };
-export type ChangeActionProps<T> = {
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>;
-    payload: T;
-};
+export type ChangeActionProps<T = undefined> = ActionProps<
+    React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+    T
+>;
+
 export type MouseActionProps<T> = {
     e: React.MouseEvent<HTMLInputElement | HTMLLIElement, MouseEvent>;
     payload: T;
@@ -43,6 +56,8 @@ export type ActionTypes =
     | 'ADD_NEW_CHARACTER'
     | 'ADD_NEW_SKILL'
     | 'LOAD_CHARACTERS'
+    | 'UPDATE_CURRENT_GUILD_ID'
+    | 'IMPORT_CHARACTERS_BY_GUILD_ID'
     | 'SAVE_CHARACTERS'
     | 'SAVE_CHARACTERS_NEWLY'
     | 'OPEN_DELETION_MODAL'
@@ -65,17 +80,19 @@ export const actions = {
     openDeletionModal: actionCreator<MouseActionProps<CharacterID>>('OPEN_DELETION_MODAL'),
     closeModal: actionCreator('CLOSE_MODAL'),
     deleteCharacter: actionCreator('DELETE_CHARACTER'),
-    deleteSkill: actionCreator<MouseActionProps<{ characterID: CharacterID; skillName: SkillName }>>(
-        'DELETE_SKILL',
-    ),
+    deleteSkill: actionCreator<MouseActionProps<{ characterID: CharacterID; skillName: SkillName }>>('DELETE_SKILL'),
     moveSkill: actionCreator<MoveSkillProps>('MOVE_SKILL'),
     copyCharacter: actionCreator<{ character: Character }>('COPY_CHARACTER'),
     openCharacterDetails: actionCreator<MouseActionProps<CharacterID>>('OPEN_CHARACTER_DETAILS'),
-    updateCurrentNewCharacterName: actionCreator<React.ChangeEvent<HTMLInputElement>>('UPDATE_CURRENT_NEW_CHARACTER_NAME'),
+    updateCurrentNewCharacterName: actionCreator<React.ChangeEvent<HTMLInputElement>>(
+        'UPDATE_CURRENT_NEW_CHARACTER_NAME',
+    ),
     addNewCharacter: actionCreator('ADD_NEW_CHARACTER'),
     addNewSkill: actionCreator('ADD_NEW_SKILL'),
+    updateCurrentGuildId: actionCreator<ChangeActionProps>('UPDATE_CURRENT_GUILD_ID'),
     ...loadCharactersActions,
     ...saveCharactersActions,
     ...saveCharactersNewlyActions,
     ...loadSkillsCsvActions,
+    ...importCharactersByGuildIdActions,
 };
