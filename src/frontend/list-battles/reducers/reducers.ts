da6fde7772e2753ac5bbe497d9f81e9e62a9d21b
h.miyazaki <aqla114@yahoo.co.jp>
2021-05-04 12:02:05 +0900

    Fix list-battles reducer : modal 周り

--- src/frontend/list-battles/reducers/reducers.ts
+++ src/frontend/list-battles/reducers/reducers.ts
@@ -1,28 +1,59 @@
 import { reducerWithInitialState } from 'typescript-fsa-reducers';
+import { updateObject } from '../../utils/reducer-commons';
 import { actions } from '../actions/actions';
 import { BattlesListState } from '../components/battle-sessions-list';
 
 const initialState: BattlesListState = {
-    sessionName: '',
-    battlesList: [],
+    state: {
+        battlesList: [],
+    },
+    current: {
+        sessionName: '',
+        deleteSessionId: -1,
+    },
+    dom: {
+        modal: null,
+    },
 };
 
 export const tableReducer = reducerWithInitialState(initialState)
     .case(actions.doneLoadingBattleSessions, (state, props) => {
-        return { ...state, battlesList: props.result.battleSessions };
+        return updateObject(state, {
+            state: updateObject(state.state, { battlesList: props.result.battleSessions }),
+        });
     })
     .case(actions.doneCreateBattleSession, (state, props) => {
-        const battlesList = [...state.battlesList, props.result.session];
+        const battlesList = [...state.state.battlesList, props.result.session];
 
-        return { ...state, battlesList, sessionName: '' };
+        return updateObject(state, {
+            state: updateObject(state.state, { battlesList }),
+            current: updateObject(state.current, { sessionName: '' }),
+        });
     })
     .case(actions.doneDeleteBattleSession, (state, props) => {
-        const battlesList = state.battlesList.filter(b => b.id !== props.result.sessionId);
+        const battlesList = state.state.battlesList.filter(b => b.id !== props.result.sessionId);
 
-        return { ...state, battlesList };
+        return updateObject(state, {
+            state: updateObject(state.state, { battlesList }),
+            dom: updateObject(state.dom, { modal: null }),
+        });
     })
     .case(actions.updateCurrentSessionName, (state, props) => {
-        return { ...state, sessionName: props.target.value };
+        return updateObject(state, {
+            current: updateObject(state.current, { sessionName: props.target.value }),
+        });
+    })
+    .case(actions.openDeletionModal, (state, props) => {
+        return updateObject(state, {
+            dom: updateObject(state.dom, { modal: { type: 'DeletionModal' } }),
+            current: updateObject(state.current, { deleteSessionId: props.payload }),
+        });
+    })
+    .case(actions.closeDeletionModal, (state, props) => {
+        return updateObject(state, {
+            dom: updateObject(state.dom, { modal: null }),
+            current: updateObject(state.current, { deleteSessionId: -1 }),
+        });
     })
     .default(state => {
         console.log('The default reducer is used.');
