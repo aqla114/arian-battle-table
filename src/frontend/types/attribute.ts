3ed1f6e92cd0dadd4a612e55b4234c87de1a20e5
h.miyazaki <aqla114@yahoo.co.jp>
2022-01-03 11:39:36 +0900

    Add DamageAttribute : 貫通とか物理とか

--- src/frontend/types/attribute.ts
+++ src/frontend/types/attribute.ts
@@ -9,3 +9,11 @@ export const attributeLabels: { [key in Attribute]: string } = {
     Light: '光',
     Darkness: '闇',
 };
+
+export type DamageAttributeRaw = Attribute | 'Physic' | 'Penetrate';
+
+export const damageAttributeLabels: { [key in DamageAttributeRaw]: string } = {
+    ...attributeLabels,
+    Physic: '物理',
+    Penetrate: '貫通',
+};
